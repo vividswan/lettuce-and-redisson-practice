218c143d7324b2cf1ee0ca4d86679c77201e7e89
vividswan <vividswan@naver.com>
2023-12-06 09:56:48 +0900

    project init

---
+++ src/main/java/com/vividswan/redispractice/RedispracticeApplication.java
@@ -0,0 +1,13 @@
+package com.vividswan.redispractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedispracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedispracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vividswan/redispractice/RedispracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vividswan.redispractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedispracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
